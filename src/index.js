b12b512facf7d0ef38603155cd80cfefeacfd387
odini <odini@injeinc.co.kr>
2022-11-08 00:08:30 +0900

    .

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import { Controller } from "./controller/Controller";
 
 window.controller = new Controller(document.querySelector("#uid_app"));
+window.controller2 = new Controller(document.querySelector("#uid_app2"));
+window.controller3 = new Controller(document.querySelector("#uid_app3"));
